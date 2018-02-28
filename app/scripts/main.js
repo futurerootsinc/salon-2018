03fb1a9a01c66e6aee6841dd5545b308abaf76d8
Saeed Abbaspour <hi@saeedreza.me>
2018-02-28 03:35:26 -0800

    Add intro and about sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,12 @@
 jQuery(document).ready(function($) {
 
+	// Initiate AOS animatin library
+	AOS.init({
+		disable: function () {
+			// disable for mobile
+			var maxWidth = 1024;
+			return window.innerWidth < maxWidth;
+		}
+	});
+
 });
